14ba3af539fc0f75c66c97a532f6deced5ba0614
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-07 18:21:59 +0900

    dd

--- ph_1/src/main.js
+++ ph_1/src/main.js
@@ -1,28 +1,17 @@
-import { Boot } from './scenes/Boot';
-import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
+import * as ph from 'phaser';
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+var mainScene = new ph.Scene("mainScene");
+
+mainScene.preload = function(){
+    console.log("main scene");
+}
+
 const config = {
-    type: Phaser.AUTO,
+    type: ph.AUTO,
     width: 1024,
     height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        Game,
-        GameOver
-    ]
+    scece: [mainScene],
 };
 
-export default new Phaser.Game(config);
+var game = new ph.Game(config);
+
